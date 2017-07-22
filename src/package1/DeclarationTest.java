a7ac05eae829cfbec8a05cbab898d57c5a4f171a
Hari Prasad <Hari Prasad@DESKTOP-S27E63D>
2017-07-22 11:28:15 +0530

    First Commit

---
+++ src/package1/DeclarationTest.java
@@ -0,0 +1,23 @@
+package package1;
+
+public class DeclarationTest {
+
+	public static void main(String[] args) {
+		int a=10;
+	       int b=20;
+	       
+	       if(a<15)
+	       {
+	    	   System.out.println("The Entered Value is Less than 15");
+	       }
+	       else
+	       {
+	    	   System.out.println("The Entered Value is Greater than 15");
+	       }
+	       
+	       int c=a+b;
+	       System.out.println("The Sum of a&b is :" + c);
+
+	}
+
+}
